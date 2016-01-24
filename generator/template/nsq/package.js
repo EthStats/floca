1c6f41f5fecbde37827c042d9b3d1fabc12fa2e4
Imre Fazekas <imre.fazekas@icloud.com>
2016-01-24 14:10:57 +0100

    websocket fixes

--- generator/template/nsq/package.js
+++ generator/template/nsq/package.js
@@ -1,6 +1,6 @@
 module.exports = {
 	dependencies: {
-		"floca": "^0.9.80",
+		"floca": "^0.10.0",
 		"floca-nsq": "latest",
 		"lodash": "~3"
 	}
--- generator/template/web/package.js
+++ generator/template/web/package.js
@@ -1,10 +1,10 @@
 module.exports = {
 	dependencies: {
 		"connect-timeout": "^1.7.0",
-		"helmet": "^1.0.2",
-		"http-proxy-middleware": "^0.9.0",
+		"helmet": "^1.1.0",
+		"http-proxy-middleware": "^0.9.1",
 		"jade": "^1.11.0",
-		"serve-static": "^1.10.0"
+		"serve-static": "^1.10.2"
 	},
 	devDependencies: {
 		"hammerjs": "^2.0.6"
