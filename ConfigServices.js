a92df53c7f68483b679157963efcc465941fd010
Imre Fazekas <imre.fazekas@icloud.com>
2016-02-24 23:25:00 +0000

    lib fix

--- ConfigServices.js
+++ ConfigServices.js
@@ -4,15 +4,15 @@ var assigner = (new Assigner()).recursive(true)
 module.exports = {
 	conf: {},
 	env: function () {
-		assigner.merge( this.conf, process.env )
+		assigner.assign( this.conf, process.env )
 		return this
 	},
 	add: function ( options ) {
-		assigner.merge( this.conf, options )
+		assigner.assign( this.conf, options )
 		return this
 	},
 	argv: function ( index ) {
-		assigner.merge( this.conf, require('minimist')(process.argv.slice(index || 2)) )
+		assigner.assign( this.conf, require('minimist')(process.argv.slice(index || 2)) )
 		return this
 	},
 	file: function (files) {
@@ -20,7 +20,7 @@ module.exports = {
 		if ( !Array.isArray(files) ) files = [ files ]
 		var filesArray = files || []
 		filesArray.forEach( function ( file ) {
-			self.conf = assigner.merge( self.conf, require( file ) )
+			self.conf = assigner.assign( self.conf, require( file ) )
 		} )
 		return this
 	},
